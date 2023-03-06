17e496534712abff15ed916734472ba5d012f122
c3n9 <arseniy11041104@gmail.com>
2023-03-06 11:24:10 +0300

    add photos

--- NBA/Pages/PPhotos.xaml.cs
+++ NBA/Pages/PPhotos.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NBA.Models;
 using NBA.Services;
 namespace NBA.Pages
 {
@@ -21,60 +22,53 @@ namespace NBA.Pages
     /// </summary>
     public partial class PPhotos : Page
     {
-        int currentIndex = 0;
+        int currentPage = 0;
+        int maxPage;
+        int takePicture = 5;
         public PPhotos()
         {
-            InitializeComponent(); 
+            InitializeComponent();
             App.MainWindowInstanse.HeaderGrid.Visibility = Visibility.Visible;
             App.MainWindowInstanse.Title.Text = "Photos";
+            maxPage = App.DB.Pictures.Count() / takePicture;
+            if (App.DB.Pictures.Count() % takePicture != 0)
+                maxPage++;
         }
 
         private void BPrevious_Click(object sender, RoutedEventArgs e)
         {
-            currentIndex--;
-            if (currentIndex < 0)
-                currentIndex = App.DB.Team.Count();
+            currentPage--;
+            if (currentPage < 0)
+                currentPage = 0;
             Refresh();
         }
 
         private void BNext_Click(object sender, RoutedEventArgs e)
         {
-            currentIndex++;
-            if (currentIndex > App.DB.Team.Count())
-                currentIndex = 0;
+            currentPage++;
+            if (currentPage == maxPage)
+                currentPage = maxPage - 1;
             Refresh();
         }
         private void Refresh()
         {
-            TBList.Text = Convert.ToString(currentIndex);
-            var images = App.DB.Team.ToList().Skip(currentIndex).Take(9).ToList();
-            if(images.Count == 0)
-                images = App.DB.Team.Take(9).ToList();
-            if (images.Count == 1)
-                images.AddRange(App.DB.Team.Take(2));
-            if (images.Count == 2)
-                images.AddRange(App.DB.Team.Take(3));
-            if (images.Count == 3)
-                images.AddRange(App.DB.Team.Take(4));
-            if (images.Count == 4)
-                images.AddRange(App.DB.Team.Take(5));
-            if (images.Count == 5)
-                images.AddRange(App.DB.Team.Take(6));
-            if (images.Count == 6)
-                images.AddRange(App.DB.Team.Take(7));
-            if (images.Count == 7)
-                images.AddRange(App.DB.Team.Take(8));
-            if (images.Count == 8)
-                images.AddRange(App.DB.Team.Take(9));
-            Image1.Source = MyTools.BytesToImage(images[0].Logo);
-            Image2.Source = MyTools.BytesToImage(images[1].Logo);
-            Image3.Source = MyTools.BytesToImage(images[2].Logo);
-            Image4.Source = MyTools.BytesToImage(images[3].Logo);
-            Image5.Source = MyTools.BytesToImage(images[4].Logo);
-            Image6.Source = MyTools.BytesToImage(images[5].Logo);
-            Image7.Source = MyTools.BytesToImage(images[6].Logo);
-            Image8.Source = MyTools.BytesToImage(images[7].Logo);
-            Image9.Source = MyTools.BytesToImage(images[8].Logo);
+            TBList.Text = Convert.ToString(currentPage + 1);
+            GenerateImages(App.DB.Pictures.ToList().Skip(currentPage * takePicture).Take(takePicture).ToList());
+
+
+        }
+        private void GenerateImages(List<Pictures> pictures)
+        {
+            WPPhotos.Children.Clear();
+            foreach (var picture in pictures)
+            {
+                Image image = new Image();
+                image.Source = MyTools.BytesToImage(picture.Img);
+                image.Width = 200;
+                image.Height = 200;
+                image.Margin = new Thickness(5);
+                WPPhotos.Children.Add(image);
+            }
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
